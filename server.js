a28321b19c8b0d2c3e4669db53999675ceff301e
Philip-Machar <philipmachar788@gmail.com>
2025-06-14 11:34:18 +0300

    ngrok set up

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.post("/sms", (req, res) => {
+    const {text, from, to} = req.body;
+
+    const response = `Thanks for this message : ${text}`;
+
+    const responseXml = `
+        <Response>
+            <Message>${response}</Message>
+        </Response>
+    `;
+
+    res.set("Content-Type", "text/xml");
+    res.send(responseXml);
+});
 
 app.get("/", (req, res) => {
     res.send("UjuziAI is running...")
